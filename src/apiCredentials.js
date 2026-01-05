b610f2772e9407e407785200c026215f03b654c7
claauzuma <c.zumarraga94@hotmail.com>
2026-01-05 10:33:38 -0300

    Cambios

--- src/apiCredentials.js
+++ src/apiCredentials.js
@@ -1,4 +1,5 @@
 // src/apiCredentials.js
-export const API_BASE = "http://192.168.1.38:8080"; 
+export const API_BASE = "https://zumafitapp-backend-production.up.railway.app/"; 
+// "http://192.168.1.38:8080"
 // ✅ En DEV: ""  -> usa /api y lo agarra el proxy
 // ✅ En PROD: "https://tu-backend.railway.app"
